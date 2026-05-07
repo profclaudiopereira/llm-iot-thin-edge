350192b432d9d6b1105cc101018a42777d7667c0
profclaudiopereira <profclaudiosilva@gmail.com>
2026-05-07 15:50:54 -0300

    feat: phase 01 esp-idf foundation setup

---
+++ firmware/phase_01_wifi/phase_01_wifi/main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+    printf("Phase 01 - WiFi Foundation\n");
+
+    while (1)
+    {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
